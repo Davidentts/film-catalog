1c76751d577f77b968dfcf9fca07467d4fa7638f
Davidentt <Davidentt@yandex.ru>
2025-10-27 12:51:34 +0300

    Added new POST view for added films

--- film-catalog-source/api/api_v1/film_catalog/views.py
+++ film-catalog-source/api/api_v1/film_catalog/views.py
@@ -1,6 +1,16 @@
+import datetime
 from typing import Annotated
+from random import randint
 
-from fastapi import Depends, APIRouter
+from annotated_types import Len
+
+from fastapi import (
+    Depends,
+    APIRouter,
+    status,
+    Form,
+)
+from pydantic import Field
 
 from .crud import film_examples
 from .dependencies import get_film_by_id
@@ -22,3 +32,33 @@ def read_film(
     ],
 ) -> Film:
     return movie
+
+
+@router.post(
+    "/",
+    response_model=Film,
+    status_code=status.HTTP_201_CREATED,
+)
+def create_film(
+    name: Annotated[
+        str,
+        Len(min_length=1, max_length=129),
+        Form(),
+    ],
+    description: Annotated[
+        str,
+        Len(min_length=10, max_length=300),
+        Form(),
+    ],
+    date_of_create: Annotated[
+        datetime.date,
+        Field(le=datetime.date.today(), description="Date of creation"),
+        Form(),
+    ],
+) -> Film:
+    return Film(
+        movie_id=randint(3, 1000),
+        name=name,
+        description=description,
+        date_of_creation=date_of_create,
+    )
