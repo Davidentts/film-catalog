d5898deb1518fec07b1be25427a5ea15b42f7b94
Davidentt <Davidentt@yandex.ru>
2026-05-12 19:57:22 +0300

    added test for generate and save tokens

---
+++ film-catalog-source/testing/test_api/test_api_v1/test_auth/test_services/test_redis_token_helper.py
@@ -0,0 +1,18 @@
+from os import getenv
+from unittest import TestCase
+
+from api.api_v1.auth.services import redis_tokens
+
+if getenv("TESTING") != "1":
+    message = "Environment is not ready for testing"
+    raise OSError(message)
+
+
+class RedisTokenHelperTestCase(TestCase):
+    def test_generate_and_save_token(self) -> None:
+        new_token = redis_tokens.generate_and_save_token(nbytes=10)
+        expected_exists = True
+        self.assertTrue(
+            expected_exists,
+            redis_tokens.token_exists(new_token),
+        )
