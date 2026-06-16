f1bbcb51471fc2102378d82d7b7dc96d49279385
Davidentt <Davidentt@yandex.ru>
2026-06-16 22:23:18 +0300

    added test for partial update synopsis

---
+++ film-catalog-source/testing/test_api/test_api_v1/test_film_catalog/test_details_views/test_partial_update.py
@@ -0,0 +1,63 @@
+from collections.abc import Generator
+
+import pytest
+from _pytest.fixtures import SubRequest
+from fastapi import status
+from fastapi.testclient import TestClient
+
+from api.api_v1.film_catalog.crud import storage
+from main import app
+from schemas.movie import SYNOPSIS_MAX_LENGTH, Movie
+from testing.conftest import create_movie
+
+
+class TestUpdatePartial:
+
+    @pytest.fixture()
+    def movie(self, request: SubRequest) -> Generator[Movie]:
+        slug, synopsis = request.param
+        movie = create_movie(slug, synopsis)
+        yield movie
+        storage.delete(movie)
+
+    @pytest.mark.parametrize(
+        "movie, new_synopsis",
+        [
+            pytest.param(
+                ("foo_part", "some synopsis"),
+                "",
+                id="some-synopsis-to-no-synopsis",
+            ),
+            pytest.param(
+                ("bar_part", ""),
+                "some synopsis",
+                id="no-synopsis-to-some-synopsis",
+            ),
+            pytest.param(
+                ("ham_part", "a" * SYNOPSIS_MAX_LENGTH),
+                "",
+                id="max-synopsis-to-no-synopsis",
+            ),
+            pytest.param(
+                ("spam_part", ""),
+                "a" * SYNOPSIS_MAX_LENGTH,
+                id="no-synopsis-to-max-synopsis",
+            ),
+        ],
+        indirect=["movie"],
+    )
+    def test_movie_update_partial(
+        self,
+        movie: Movie,
+        new_synopsis: str,
+        auth_client: TestClient,
+    ) -> None:
+        url = app.url_path_for("update_movie_details_partial", slug=movie.slug)
+        response = auth_client.patch(
+            url,
+            json={"synopsis": new_synopsis},
+        )
+        assert response.status_code == status.HTTP_200_OK, response.text
+        movie_db = storage.get_by_slug(movie.slug)
+        assert movie_db
+        assert movie_db.synopsis == new_synopsis, movie_db
